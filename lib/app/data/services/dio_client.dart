f5e91e789eb1c4cbdf0950999748830ae22cd819
Noufal Ibrahim <noufal.github@gmail.com>
2025-07-24 01:01:01 +0700

    edit dio_client

--- lib/app/data/services/dio_client.dart
+++ lib/app/data/services/dio_client.dart
@@ -1,14 +1,33 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 class DioClient {
-  static final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: 'https://open-api.my.id/api/quran',
-      connectTimeout: const Duration(seconds: 15),
-      receiveTimeout: const Duration(seconds: 15),
-      responseType: ResponseType.json,
-    ),
-  );
+  static final Dio _dio =
+      Dio(
+          BaseOptions(
+            baseUrl: 'https://open-api.my.id/api/quran',
+            connectTimeout: const Duration(seconds: 15),
+            receiveTimeout: const Duration(seconds: 15),
+            responseType: ResponseType.json,
+          ),
+        )
+        ..interceptors.addIf(
+          kDebugMode,
+          LogInterceptor(
+            request: true,
+            requestBody: true,
+            responseBody: true,
+            responseHeader: false,
+            error: true,
+            requestHeader: false,
+          ),
+        );
 
   static Dio get instance => _dio;
+}
+
+extension on Interceptors {
+  void addIf(bool condition, Interceptor interceptor) {
+    if (condition) add(interceptor);
+  }
 }
